036ac8e250fdebc1c053cc03896cad3085e81e93
fsica0308 <fsica0308@gmail.com>
2024-09-17 20:42:51 -0300

    Arreglos en JSON, funcion leer archivo, y funcion Alta de producto terminada

--- TP_ElKoto.py
+++ TP_ElKoto.py
@@ -4,6 +4,9 @@ import json
 Anotaciones:
 
 Promociones: 2x1 se guarda como texto "2x1", descuentos se guarda el % ej: 45, descuentos en x unidad se guarda ej: 25% en 2 unidad -> 252
+Ademas las promociones son unicamente enteros. En caso de no tener promocion se indica con un 0.
+
+JSON: Todo lo que se ingrese al JSON debe ingresarse sin tildes.
 '''
 
 #Funcion Menu Inicial
@@ -27,17 +30,20 @@ def menu_principal():
     return opcion
 
 def leer_archivo():
-    archivo = open("TP_ElKoto_Algoritmos_I/productos.json", "r")
-    lineas_productos = archivo.read()
-    archivo.close()
+    try:
+        archivo = open("TP_ElKoto_Algoritmos_I/productos.json", "r")
+        lineas_productos = archivo.read()
+        archivo.close()
     
-    if len(lineas_productos) == 0:
-        print("El archivo JSON está vacío o no contiene datos.")
-        return
+        if len(lineas_productos) == 0:
+            print("El archivo JSON está vacío o no contiene datos.")
+            return
     
-    productos = json.loads(lineas_productos)
-    
-    return productos
+        productos = json.loads(lineas_productos)
+        return productos
+        
+    except:
+        print("No se puede abrir el archivo productos")
 
 def menu_info_productos():
     print()
@@ -136,9 +142,11 @@ def menu_abm_productos():
     while opcion < 1 or opcion > 4:
         opcion = int(input("Error. Ingrese una opcion correcta: "))
         
+    productos = leer_archivo()
+        
     while opcion >= 1 and opcion <= 3:
         if opcion == 1:
-            print("alta prod")
+            alta_producto(productos)
         elif opcion == 2:
             print("baja prod")
         else:
@@ -151,6 +159,93 @@ def menu_abm_productos():
             opcion = int(input("Error. Ingrese una opcion correcta: "))     
     else:
         return
+    
+def alta_producto(productos):
+    print("Datos del Producto: ")
+    # Obtener el diccionario con el valor máximo de "id"
+    maximo_id = max(productos, key=obtener_id)
+    id_aux = maximo_id["id"]
+    id = id_aux + 1
+    
+    nombre = input("\tIngrese el nombre: ")
+    marca = input("\tIngrese la marca: ")
+    # Variable para saber si encontramos el nombre
+    encontrado = False
+
+    # Recorrer la lista y verificar si algún diccionario tiene el nombre y marca buscado
+    for diccionario in productos:
+        if diccionario['nombre'].lower() == nombre.lower() and diccionario['marca'].lower() == marca.lower():
+            encontrado = True
+            break
+        
+    while encontrado:
+        print("El producto ya existe, ingrese otro por favor: ")
+        nombre = input("\tIngrese el nombre: ")
+        marca = input("\tIngrese la marca: ")
+        encontrado = False
+        for diccionario in productos:
+            if diccionario['nombre'].lower() == nombre.lower() and diccionario['marca'].lower() == marca.lower():
+                encontrado = True
+                break
+        
+    precio = float(input("\tIngrese el precio (sin signos): "))
+    while precio <= 0:
+        precio = float(input("\tError. Ingrese el precio (sin signos): "))
+        
+    ubicacion = input("\tIngrese la ubicacion: ")
+    
+    stock = int(input("\tIngrese el stock: "))
+    while stock < 1:
+        stock = int(input("\tError. Ingrese el stock: "))
+    
+    print("\t¿Desea agregar una promocion? \n\t\t1.Si \n\t\t2.No")
+    print()
+    promo = int(input("\t\tOpcion: "))
+    while promo < 1 or promo > 2:
+        promo = int(input("\t\tError. Ingrese una opcion correcta: ")) 
+        
+    if promo == 1:
+        print("\tQue promocion desea agregar (ejemplos): \n\t\t1.NxM \n\t\t2.N'%' en la M unidad \n\t\t3.N'%'")
+        print()
+        promo_opcion = int(input("\t\tOpcion: "))
+        while promo_opcion < 1 or promo_opcion > 3:
+            promo_opcion = int(input("\t\tError. Ingrese una opcion correcta: ")) 
+        if promo_opcion == 1:
+            valor_1 = int(input("Ingrese el primer valor: "))
+            while valor_1 < 1:
+                valor_1 = int(input("Error. Ingrese el primer valor: "))
+            valor_2 = int(input("Ingrese el segundo valor: "))
+            while valor_2 < 1:
+                valor_2 = int(input("Error. Ingrese el segundo valor: "))
+            promocion = valor_1 + "x" + valor_2
+        elif promo_opcion == 2:
+            valor_1 = int(input("Ingrese el primer valor: "))
+            while valor_1 < 1 or valor_1 > 99:
+                valor_1 = int(input("Error. Ingrese el primer valor: "))
+            valor_2 = int(input("Ingrese el segundo valor: "))
+            while valor_2 < 1:
+                valor_2 = int(input("Error. Ingrese el segundo valor: "))
+            promocion = str(valor_1) + str(valor_2)
+        else:
+            valor_1 = int(input("Ingrese el valor: "))
+            while valor_1 < 1 or valor_1 > 99:
+                valor_1 = int(input("Error. Ingrese el valor: "))
+    else:
+        promocion = "0"
+    
+    productos.append({"id": id, "nombre": nombre, "marca": marca, "precio": precio, "ubicacion": ubicacion, "stock": stock, "promocion": promocion})
+    productosJSON = json.dumps(productos, indent=4)
+    try:
+        archivo = open("TP_ElKoto_Algoritmos_I/productos.json", "w")
+        archivo.write(productosJSON)
+        archivo.close()
+        print("Producto agregado!")
+    except:
+        print("No se puede grabar el archivo productos")
+        
+# Función para obtener el valor del campo "id"
+def obtener_id(elemento):
+    return elemento['id']
 
 def menu_salir():
     banner = """
